893872500636f92f652bde0421130c551b99ac21
thnguyen <tronnguyen@deloitte.com.au>
2017-12-19 02:00:30 +1100

    1st sign up, user will always allowed to login

--- src/main/resources/rules/enable_progressive_profile.js
+++ src/main/resources/rules/enable_progressive_profile.js
@@ -1,7 +1,9 @@
 function (user, context, callback) {
   user.user_metadata = user.user_metadata || {};
-  if (!user.given_name && !user.family_name && !user.user_metadata.given_name && !user.user_metadata.family_name) {
-    context.idToken['https://hiddencharm.auth0.com/profile'] = 'incomplete';
+  if (context.stats.loginsCount > 1) { // for clarity: never profile for sign up
+    if (!user.given_name && !user.family_name && !user.user_metadata.given_name && !user.user_metadata.family_name) {
+      context.idToken['https://hiddencharm.auth0.com/profile'] = 'incomplete';
+    }
   }
   callback(null, user, context);
 }
